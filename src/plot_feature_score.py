b3dad3e48a7fec5dc5d20a801d96f79a213825e9
Chinmayee Shah <chshah@stanford.edu>
2015-11-12 10:48:54 -0800

    pass at milestone intro

--- src/plot_feature_score.py
+++ src/plot_feature_score.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 import re
 import utils
 from windowModel import WindowModel
+    
+"""
+Plot each token's score in predicting/ contributing to next token.
+"""
+def plotFeatureScore(score, fname):
+    winSize = len(score)
+    fig, ax = plt.subplots()
+    plt.plot(range(winSize, 0, -1), score)
+    plt.savefig(fname)
 
 class WindowTokenFeatureStrength(WindowModel):
     """
@@ -11,7 +21,7 @@ class WindowTokenFeatureStrength(WindowModel):
     predicting the next token.
     """
 
-    def __init__(self, keywords, winSize=100, scale=0.02):
+    def __init__(self, keywords, winSize=100, scale=0.01):
         super(WindowTokenFeatureStrength, self).__init__(keywords, winSize=winSize)
         self.scale=0.02
 
@@ -19,7 +29,9 @@ class WindowTokenFeatureStrength(WindowModel):
     Compute correlation based score between next token and each token in the
     window.
     """
-    def computeCorrelation(self, filesAndWords):
+    def computeCorrelation(self, filesAndWords, tmpdir):
+        if not os.path.exists(tmpdir):
+            os.makedirs(tmpdir)
         exy = np.zeros(self.winSize)
         ex  = np.zeros(self.winSize)
         ey  = 0
@@ -44,20 +56,15 @@ class WindowTokenFeatureStrength(WindowModel):
                 sumxy = sumxy + window * target
                 sumx  = sumx  + window
                 sumy  = sumy  + target
-            if fid % 1000 == 0:
-                print 'Finished reading %d files' % i
-        exy = exy + sumxy/float(numWindows)
-        ex  = ex + sumx/float(numWindows)
-        ey  = ey + sumy/float(numWindows)
-        return abs(exy - ex * ey)
-    
-"""
-Plot each token's score in predicting/ contributing to next token.
-"""
-def plotFeatureScore(score, fname):
-    winSize = len(score)
-    plt.plot(range(winSize, 0, -1), score)
-    plt.savefig(fname)
+            if fid % 500 == 0:
+                print 'Finished computing %d files' % fid
+            exy = exy + sumxy/float(numWindowsTotal)
+            ex  = ex + sumx/float(numWindowsTotal)
+            ey  = ey + sumy/float(numWindowsTotal)
+            score = abs(exy - ex * ey)
+            if fid % 2000 == 0:
+                plotFeatureScore(score, '%s/correlation_%d' % (tmpdir, fid))
+        return score
 
 if __name__ == '__main__':
     keywordfile = '../key_words/c'
@@ -68,12 +75,23 @@ if __name__ == '__main__':
             keywords.append(kw)
     # NOTE: edit the following line to change the files input to compute
     # feature score
-    datafiles = utils.matchingFiles(['../data/linux'], ['c', 'h'])[20000:30000]
-    filesAndWords = []
-    for i,name in enumerate(datafiles):
+    trainfiles = utils.matchingFiles(['../data/linux'], ['c', 'h'])[0:20000]
+    trainFilesAndWords = []
+    for i,name in enumerate(trainfiles):
         if i % 1000 == 0:
             print 'Finished reading %d files' % i
-            filesAndWords.append((name,utils.tokenize(name)))
+        trainFilesAndWords.append((name,utils.tokenize(name)))
     featureStrength = WindowTokenFeatureStrength(keywords)
-    corr = featureStrength.computeCorrelation(filesAndWords)
-    plotFeatureScore(corr, 'example_results/correlation')
+    corr = featureStrength.computeCorrelation(trainFilesAndWords, 'example_results/tmp_train')
+    plotFeatureScore(corr, 'example_results/correlation_train')
+    testfiles = utils.matchingFiles(['../data/linux'], ['c', 'h'])[20000:]
+    print("Finished computing correlation on train data")
+    testFilesAndWords = []
+    for i,name in enumerate(testfiles):
+        if i % 1000 == 0:
+            print 'Finished reading %d files' % i
+        testFilesAndWords.append((name,utils.tokenize(name)))
+    featureStrength = WindowTokenFeatureStrength(keywords)
+    corr = featureStrength.computeCorrelation(testFilesAndWords, 'example_results/tmp_test')
+    plotFeatureScore(corr, 'example_results/correlation_test')
+    print("Finished computing correlation on test data")
